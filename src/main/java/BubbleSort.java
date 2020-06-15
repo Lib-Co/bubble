2916b4b0e69b295e8da9ac42d24dc94d5ca52c22
Libby Copsey <libbycopsey@hotmail.co.uk>
2020-06-15 23:56:49 +0100

    Final commit

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -2,28 +2,30 @@ import java.io.*;
 import java.util.Arrays;
 
 public class BubbleSort {
-    public static void main(String[] args) throws Exception {
-        File names = new File("/home/libby/Dev/AP/resources/Names/names.txt");
-//        String[] arr = {"test", "hello", "bye", "matt", "libby"};
+    public static void main(String[] args) {
+        //Read in names from file and store in an array as one string
+        File names = new File("names.txt");
         String[] arr = new String[0];
+        //Remove speech marks and split the imported string into names as separated by commas
         try {
             BufferedReader in = new BufferedReader(new FileReader(names));
             String line = in.readLine();
             line = line.replace("\"", "");
-            arr = line.split("," );
+            arr = line.split(",");
 
             System.out.println("Names to sort: " + Arrays.toString(arr));
         } catch (IOException e) {
             System.out.println(e);
         }
 
-        boolean swapped;
+        //Bubble sort functionality
         String temp;
         int n = arr.length;
-        //set range of loop to be cover all values in array list
+        //Iterate over array until no further swaps are required
         for (int j = 0; j < n; j++) {
+            //Compare adjacent values until the end of the array is reached
             for (int i = 0; i < (n - j - 1); i++) {
-                if (arr[i].compareTo(arr[i + 1]) > 0) {
+                if (compareValues(arr[i], arr[i + 1]) > 0) {
                     temp = arr[i + 1];
                     arr[i + 1] = arr[i];
                     arr[i] = temp;
@@ -32,4 +34,24 @@ public class BubbleSort {
         }
         System.out.println("Sorted names: " + Arrays.toString(arr));
     }
+
+    //Reimplemented String.compareTo method
+    public static int compareValues(String first, String second) {
+        byte[] v1 = first.getBytes();
+        byte[] v2 = second.getBytes();
+        int len1 = first.length();
+        int len2 = second.length();
+        int lim = Math.min(len1, len2);
+        for (int k = 0; k < lim; k++) {
+            if (v1[k] != v2[k]) {
+                return getChar(v1, k) - getChar(v2, k);
+            }
+        }
+        return len1 - len2;
+    }
+
+    public static char getChar(byte[] val, int index) {
+        return (char) (val[index] & 0xff);
+    }
 }
+
